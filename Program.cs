241fd9656e1bb92d32b445aca2ae487526e69c22
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-16 16:54:11 -0600

    Added a string variable

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@ namespace myApp
 {
     class Program
     {
+      //
         static void Main(string[] args)
         {
+            string name = "Mateo Rosenberg";
             Console.WriteLine("Hello World!");
-      Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("The current time is " + DateTime.Now);
+          
         }
     }
 }

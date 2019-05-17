377f8b03e5ccfd536b4a9320ab8261943ac08005
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-17 15:47:27 -0600

    Added an int variable

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace myApp
         static void Main(string[] args)
         {
             string name = "Mateo Rosenberg";
+            int age = 35;
+
             Console.WriteLine("Hello" + name );
             Console.WriteLine("The current time is " + DateTime.Now);
           

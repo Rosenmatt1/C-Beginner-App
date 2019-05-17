0df7353f425c24f14f1ef8de8268b16b6d240e28
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-17 16:07:30 -0600

    Added 3 new string varaibales to describe app

--- Program.cs
+++ Program.cs
@@ -7,12 +7,16 @@ namespace myApp
       //
         static void Main(string[] args)
         {
-            string name = "Mateo Rosenberg";
-            int age = 35;
+            // string name = "Mateo Rosenberg";
+            // int age = 35;
+            // Console.WriteLine(name + " is " + age );
+            // Console.WriteLine("{0} is {1}", name, age);
+            // Console.WriteLine("The current time is " + DateTime.Now);
 
-            Console.WriteLine(name + " is " + age );
-            Console.WriteLine("{0} is {1}", name, age);
-            Console.WriteLine("The current time is " + DateTime.Now);
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Matthew Rosenberg";
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
           
         }
     }
